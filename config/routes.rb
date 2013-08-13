f7cc27ff26b6b5e304958e1e00882b6c074cf631
Ivan Piliaiev <piliaiev@gmail.com>
2013-08-13 15:34:12 +0600

    Мелкие правки.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 GpbMerchant::Engine.routes.draw do
 
-  get "payments/gpb/check"  => 'gpb#check'
+  get "gpb/check"  => 'gpb#check'
 
-  get "payments/gpb/pay"    => 'gpb#pay'
+  get "gpb/pay"    => 'gpb#pay'
 
 end # draw
